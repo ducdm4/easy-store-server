19378cb4cc8823e66b6c1930b56ed3d219dba264
Duc Duong Minh <duc.duongminh@hermes-europe.co.uk>
2024-08-30 17:30:24 +0700

    fix alert

--- src/notification/notifications.service.ts
+++ src/notification/notifications.service.ts
@@ -3,17 +3,9 @@ import { NotificationEntity } from 'src/typeorm/entities/notification.entity';
 import { Repository } from 'typeorm';
 import * as firebaseAdmin from 'firebase-admin';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD80ARblxY_LnRznwmySo-WExRKx_OdOJc',
-  authDomain: 'delivery-system-ccad9.firebaseapp.com',
-  projectId: 'delivery-system-ccad9',
-  storageBucket: 'delivery-system-ccad9.appspot.com',
-  messagingSenderId: '978808956556',
-  appId: '1:978808956556:web:1c0221228adc9a98787da9',
-  measurementId: 'G-QKTK3XVXGE',
-};
-
-firebaseAdmin.initializeApp(firebaseConfig);
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert('firebase.json'),
+});
 
 @Injectable()
 export class NotificationsService {
